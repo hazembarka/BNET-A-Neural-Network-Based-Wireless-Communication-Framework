ffa48a15ac1ffbce692c467ff04f91b8cb664eb4
Hazem Barka <49459611+hazembarka@users.noreply.github.com>
2025-01-22 17:29:59 -0500

    Update MAPdecoding_TSMGaussianNoise_Real_BPSK.m

--- MAPdecoding_TSMGaussianNoise_Real_BPSK.m
+++ MAPdecoding_TSMGaussianNoise_Real_BPSK.m
@@ -1,4 +1,4 @@
 function symbols_proba = MAPdecoding_TSMGaussianNoise_Real_BPSK(y,modulation_alphabet,mod_alphabet_apriori,trans_mat,init_state_prob,GaussPDFmean,GaussPDFvariance)
 
-
+%% Not available because of an NDA/IP agreement with the industrial partner
 return;
